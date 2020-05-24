ce32da5605055b825e3400fb95279a4e873c2666
sidmonta <sidmonta.15@gmail.com>
2020-05-24 12:16:06 +0200

    Add the CachePlugin service for the project

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,6 @@
 import * as Tools from './tools'
 import * as Lod from './lods'
 import * as Rx from './stream'
+import * as Services from './services'
 
-export { Tools, Lod, Rx }
+export { Tools, Lod, Rx, Services }
