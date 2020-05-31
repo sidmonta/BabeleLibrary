60f08c685bc2695bfb7d15e7e6f0d2dec1c2ad35
sidmonta <sidmonta.15@gmail.com>
2020-05-31 11:04:53 +0200

    Replace Rxjs/ajax with Axios

--- lib/stream/index.ts
+++ lib/stream/index.ts
@@ -1,10 +1,20 @@
-import { ajax, AjaxResponse } from 'rxjs/ajax'
+import { AjaxResponse } from 'rxjs/ajax'
 import { Observable, of } from 'rxjs'
 import { map, switchMap, filter, catchError } from 'rxjs/operators'
 import { path, hasPath } from 'ramda'
+import axios, { AxiosResponse } from 'axios'
 
 import QuadFactory from '../lods/QuadFactory'
 import { Quad, DataFactory } from 'n3'
+
+/**
+ * Tipo di ritorno di una richiesta fetch
+ */
+export interface FetchResponse {
+    response: AxiosResponse,
+    body: any,
+    params: any
+}
 
 /**
  * Trasforma uno stream di dati in un Observable
@@ -13,7 +23,7 @@ import { Quad, DataFactory } from 'n3'
  * @param dataEventName nome del metodo dello stream per l'arrivo di nuovi dati. Di default 'data'
  * @returns {Observable<any>} Observable dello stream
  */
-export function fromStream(
+export function fromStream (
   stream: any,
   finishEventName = 'end',
   dataEventName = 'data'
@@ -21,15 +31,15 @@ export function fromStream(
   stream.pause()
 
   return new Observable(observer => {
-    function dataHandler(data: any) {
+    function dataHandler (data: any) {
       observer.next(data)
     }
 
-    function errorHandler(err: any) {
+    function errorHandler (err: any) {
       observer.error(err)
     }
 
-    function endHandler() {
+    function endHandler () {
       observer.complete()
     }
 
@@ -47,28 +57,44 @@ export function fromStream(
   })
 }
 
-const checkContentType = (data: AjaxResponse) =>
+export function fetchContent ({ url, params }: any): Observable<FetchResponse> {
+  return new Observable((observer) => {
+    axios(url)
+      .then((response: AxiosResponse) => {
+        observer.next({
+          response: response, body: response.data, params
+        })
+        observer.complete()
+      })
+      .catch(err => { observer.error(err) })
+  })
+}
+
+const checkContentType = (data: FetchResponse) =>
   Boolean(
-    hasPath(['response', 'headers', 'Content-Type'], data) &&
-    (data.response.headers['Content-Type'].includes('xml') ||
-      data.response.headers['Content-Type'].includes('rdf'))
+    hasPath(['response', 'headers', 'content-type'], data) &&
+    (data.response.headers['content-type'].includes('xml') ||
+      data.response.headers['content-type'].includes('rdf'))
   )
 
 /**
  * Esegue una chiamata fetch. Specifico per gli endpoint SPARQL, restituisce un RDF
- * @param {string} uri uri del record da cui ottenere l'RDF
+ * @param {string} url uri del record da cui ottenere l'RDF
  * @returns {Observable<Quad>} Observable di quad
  */
-export function fatchSPARQL(url: string): Observable<Quad> {
-  return ajax({
-    url,
-    method: 'GET',
-    headers: {
-      Accept: 'application/rdf+xml'
+export function fatchSPARQL (url: string): Observable<Quad> {
+  console.log('Fetch', url)
+  return fetchContent({
+    url: {
+      url,
+      method: 'GET',
+      headers: {
+        Accept: 'application/rdf+xml'
+      }
     }
   }).pipe(
     filter(checkContentType),
-    map(path(['response', 'data'])),
+    map(path(['body'])),
     switchMap((data: unknown) =>
       QuadFactory.generateFromString(data as string, true)
     ),
