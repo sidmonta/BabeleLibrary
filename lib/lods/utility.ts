1a740d5ce2f143c5d384c06c14e3685c7587a5ba
sidmonta <sidmonta.15@gmail.com>
2020-06-27 11:15:55 +0200

    Add utility for verify if a URI point to LOD resource

--- lib/lods/utility.ts
+++ lib/lods/utility.ts
@@ -1,7 +1,9 @@
-import { curry, last, split, pipe } from 'ramda'
+import { curry, last, split, pipe, hasPath } from 'ramda'
 import { parse } from 'url'
 import { Quad } from 'n3'
 import { trimCh } from '../tools'
+import axios from 'axios'
+import { allCheck } from './changeUri'
 
 /**
  * Estrapola l'identificativo dall'URI di un elemento
@@ -20,8 +22,8 @@ export const checkQuad = curry((fil: string, quad: Quad): boolean => {
   const regex = new RegExp(fil, 'gi')
   return Boolean(
     quad?.object?.value?.match(regex) ||
-      quad?.predicate?.value?.match(regex) ||
-      quad?.subject?.value?.match(regex)
+    quad?.predicate?.value?.match(regex) ||
+    quad?.subject?.value?.match(regex)
   )
 })
 
@@ -37,3 +39,26 @@ export const fixedEncodeURIComponent = (str: string): string =>
  */
 export const formUrlEncoded = (x: {}): string =>
   Object.keys(x).reduce((p, c) => p + `&${c}=${encodeURIComponent(x[c])}`, '')
+
+/**
+ * Verifica se l'URI passata come parametro è una risorsa LOD.
+ * Il controllo avviene controllando il content-type della risposta alla chiamata alla risorsa. Si è utilizzato il
+ * metodo HEAD per risparmiare banda e tempo nella risposta visto che del contenuto non occorre
+ * @param uri URI da cui controllare
+ * @return se l'uri identifica una risorsa LOD
+ */
+export const isLodURI: (uri: string) => Promise<boolean> = async (uri: string) => {
+  return axios.head(allCheck(uri), {
+    headers: {
+      Accept: 'application/rdf+xml'
+    }
+  }).then(response => {
+    return Boolean(
+      hasPath(['headers', 'content-type'], response) &&
+      (
+        response.headers['content-type'].includes('xml') ||
+        response.headers['content-type'].includes('rdf')
+      )
+    )
+  })
+}
