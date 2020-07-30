2ec50887def197fbc9cc50ed424b45f4b4dda0f7
sidmonta <sidmonta.15@gmail.com>
2020-07-30 22:51:35 +0200

    Add utility type for project

--- lib/index.ts
+++ lib/index.ts
@@ -1,15 +1,5 @@
-// import * as Tools from './tools'
-// import * as Lod from './lods'
-// import * as Rx from './stream'
-// import * as Services from './services'
-// import type { NonEmptyArray} from './tools'
-//
-// export { Tools, Lod, Rx, Services }
-// export type {
-//     NonEmptyArray as Types.NonEmpyArray
-// }
-
 export * as Tools from './tools'
 export * as Lod from './lods'
 export * as Rx from './stream'
 export * as Services from './services'
+export * as Types from './types'
